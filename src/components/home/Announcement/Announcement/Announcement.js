3684c56f2b3e1b204941ac6dbaa4534f4f774252
Programmer Sajeeb <sajeeb.programmer@gmail.com>
2022-04-19 22:10:26 +0600

    overlay complete

--- src/components/home/Announcement/Announcement/Announcement.js
+++ src/components/home/Announcement/Announcement/Announcement.js
@@ -11,8 +11,23 @@ const Announcement = (props) => {
             <Box sx={{boxShadow: 'rgba(0, 0, 0, 0.16) 0px 1px 4px'}}>
                 <Box className={styles.img}>
                     <img src={img} alt=""/>
-                    <Box className={styles.overlay}>
-                        <p>zb kjsv ushv oDH OVDH JOIV</p>
+                    <Box sx={{display:'flex', justifyContent:'center', alignItems:'center' }} className={styles.overlay}>
+                        <Link to='/'>
+                            <Box sx={{width:'90%', margin:'0 auto'}}>
+                                <Box>
+                                <Box sx={{display:'flex', justifyContent:'center', alignItems:'center'}}>
+                                    <Link className={styles.overlayIcon} to='/'><i class="fas fa-link"></i></Link>
+                                    <Link className={styles.overlayIcon} to='/'><i class="fas fa-search"></i></Link>
+                                </Box>
+                                <Typography variant="h2" component="h2" className={styles.overlayTitle}>
+                                    May 2021 Established Luminous Co., Ltd.
+                                </Typography>
+                                <Link to='/' style={{display:'block',textAlign:'center', color:'#212934', fontSize:'13px'}}>
+                                    news
+                                </Link> 
+                                </Box>
+                            </Box>
+                        </Link>
                     </Box>
                 </Box>
                 <Box sx={{padding:'30px 20px'}}>
