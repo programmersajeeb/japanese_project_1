3e1e048206b7b622f73e8ca33050fe0b1820a9c4
arifuzzaman arif <ac.arifarifuzzaman1@gmail.com>
2022-04-27 06:07:45 +0600

    add dashboard nestin Route

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import { Box } from '@mui/material';
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './components/home/Home/Home';
-import Mission from './components/Mission/Mission';
-import Jobs from './components/Jobs/Jobs';
-import Company from './components/CompanyProfile/Company';
-import Dashboard from './components/dashboard/Dashboard/Dashboard';
+import { Box } from "@mui/material";
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Company from "./components/CompanyProfile/Company";
+import AddProduct from "./components/dashboard/AddProduct/AddProduct";
+import Dashboard from "./components/dashboard/Dashboard/Dashboard";
+import MakeAdmin from "./components/dashboard/MakeAdmin/MakeAdmin";
+import Home from "./components/home/Home/Home";
+import Jobs from "./components/Jobs/Jobs";
+import Mission from "./components/Mission/Mission";
 function App() {
-	return (
-		<Box>
-			<BrowserRouter>
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/" element={<Home />} />
-					<Route path="/mission" element={<Mission />} />
-					<Route path="/job" element={<Jobs />} />
-					<Route path="/company" element={<Company />} />
-					<Route path="dashboard" element={  <Dashboard /> } />
-				</Routes>
-			</BrowserRouter>
-		</Box>
-	);
+  return (
+    <Box>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/mission" element={<Mission />} />
+          <Route path="/job" element={<Jobs />} />
+          <Route path="/company" element={<Company />} />
+          <Route path="/dashboard/*" element={<Dashboard />}>
+					<Route path="makeAdmin" element={<MakeAdmin />} />
+          <Route path="addproduct" element={<AddProduct />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Box>
+  );
 }
 
-export default App
+export default App;
